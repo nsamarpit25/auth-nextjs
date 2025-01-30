6226aa5c7a9209f858236caec8b26d2680504d97
Samarpit <nsamarpit25@gmail.com>
2025-01-31 01:49:36 +0530

    complete sign up and add toaster

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,52 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextResponse, NextRequest } from "next/server";
+import bcryptjs from "bcryptjs";
+import User from "@/models/userModel";
+
+connect();
+
+export async function POST(request: NextRequest) {
+   try {
+      const reqBody = await request.json();
+      const { username, email, password } = reqBody;
+      console.log("reached backend");
+
+      // check if user already exists
+      const user = await User.findOne({ email });
+
+      if (user) {
+         return NextResponse.json(
+            { error: "User already exists" },
+            { status: 400 }
+         );
+      }
+
+      // hash password
+      const salt = await bcryptjs.genSalt(10);
+      const hashedPassword = await bcryptjs.hash(password, salt);
+
+      const newUser = new User({
+         username,
+         email,
+         password: hashedPassword,
+      });
+
+      const savedUser = await newUser.save();
+
+      return NextResponse.json(
+         {
+            message: "User created successfully",
+            success: true,
+            savedUser,
+         },
+         { status: 201 }
+      );
+   } catch (error) {
+      console.log(error);
+      let message = "An error occurred";
+      if (error instanceof Error) {
+         message = error.message;
+      }
+      return NextResponse.json({ message, success: false });
+   }
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,59 @@
 "use client";
+import axios, { AxiosError } from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function SingUpPage() {
-   const [user, setUser] = useState({ username: "", email: "", password: "" });
    const router = useRouter();
+   const [user, setUser] = useState({ username: "", email: "", password: "" });
+   const [buttonDisabled, setButtonDisabled] = useState(false);
+   const [loading, setLoading] = useState(false);
 
-   async function onSubmit() {}
+   useEffect(() => {
+      if (
+         user.username.length > 0 &&
+         user.email.length > 0 &&
+         user.password.length > 0
+      ) {
+         setButtonDisabled(false);
+      } else {
+         setButtonDisabled(true);
+      }
+   }, [user]);
+
+   async function onSubmit() {
+      try {
+         setLoading(true);
+         console.log(user);
+         const response = await axios.post("/api/users/signup", user);
+         toast.success(response.data.message);
+         console.log(response.data);
+         router.push("/login");
+      } catch (error) {
+         console.error(error);
+         if (error instanceof AxiosError) {
+            toast.error(error.response?.data?.error || error.message);
+         } else {
+            toast.error("An error occurred");
+         }
+      } finally {
+         setLoading(false);
+      }
+   }
 
    return (
       <div className="flex items-center justify-center min-h-screen bg-gray-900 flex-col gap-6 p-8">
+         <button
+            onClick={() => {
+               toast.success("hello");
+            }}
+         >
+            Toast
+         </button>
          <h1 className="text-center text-white text-3xl font-bold mb-4">
-            Sign Up
+            {loading ? "processing.." : "Sign Up"}
          </h1>
          <div className="flex gap-4 flex-row items-center">
             <label
@@ -68,8 +109,9 @@ export default function SingUpPage() {
          <button
             onClick={onSubmit}
             className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-lg transition duration-300 mt-4"
+            disabled={buttonDisabled}
          >
-            Sign Up
+            {buttonDisabled ? "Fill in all fields" : "Sign Up"}
          </button>
          <Link
             href="/login"
